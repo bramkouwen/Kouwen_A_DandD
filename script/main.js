d079ddf480e4af9dbddba1afe2b099d75845ba15
bramkouwen <97710600+bramkouwen@users.noreply.github.com>
2022-05-25 03:06:47 -0400

    Updating Javascript

--- script/main.js
+++ script/main.js
@@ -1,4 +1,14 @@
 (() => {
-	// set up the puzzle pieces and boards
-	
+	// collect the buttons at the bottom of the page
+let theThumbnails = document.querySelectorAll("#buttonHolder img"),
+	gameBoard = document.querySelector(".puzzle-board");
+
+function changeBgImg() {
+	// debugger;
+	gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`
+}
+
+// add event handling here 
+
+theThumbnails.forEach(item => item.addEventListener("click", changeBgImg));
 })();
