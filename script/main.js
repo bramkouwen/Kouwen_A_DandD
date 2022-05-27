86c425c536f63785f1928e6b32d7843aa2d2db95
bramkouwen <97710600+bramkouwen@users.noreply.github.com>
2022-05-27 14:53:08 -0400

    Updating Javascript and CSS

--- script/main.js
+++ script/main.js
@@ -1,14 +1,43 @@
 (() => {
 	// collect the buttons at the bottom of the page
 let theThumbnails = document.querySelectorAll("#buttonHolder img"),
-	gameBoard = document.querySelector(".puzzle-board");
+	gameBoard = document.querySelector(".puzzle-board"), 
+	pzlPieces = document.querySelectorAll('.puzzle-pieces img'),
+	dropZones = document.querySelectorAll('.drop-zone');
 
 function changeBgImg() {
 	// debugger;
 	gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`
 }
 
+function allowDrag(event) {
+	console.log('started draggin me');
+
+	event.dataTransfer.setData('daggedEl', this.id);
+}
+
+function allowDragOver(event) {
+	event.preventDefault();
+	console.log('started draggin over me');
+}
+
+function allowDrop(event) {
+	event.preventDefault();
+	console.log('dropped on me');
+
+	let droppedElId = event.dataTransfer.getData('daggedEl');
+
+	this.appendChild(document.querySelector(`#${droppedElId}`));
+}
+
 // add event handling here 
 
 theThumbnails.forEach(item => item.addEventListener("click", changeBgImg));
+
+pzlPieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
+
+dropZones.forEach(zone => {
+	zone.addEventListener('dragover', allowDragOver);
+	zone.addEventListener('drop', allowDrop);
+});
 })();
